ee17d633d3c54cdcc7898629db8adc5e72056774
Mervin Choun <mchoun@paypal.com>
2024-10-04 15:27:44 -0400

    add paypal button to checkout

---
+++ app/providers.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js'
+import { ReactNode } from 'react'
+
+export function Providers({ children }: { children: ReactNode }) {
+  const initialOptions = {
+    clientId: 'test',
+    currency: 'USD',
+    intent: 'capture',
+  }
+  return (
+    <PayPalScriptProvider options={initialOptions}>
+      {children}
+      <PayPalButtons />
+    </PayPalScriptProvider>
+  )
+}
